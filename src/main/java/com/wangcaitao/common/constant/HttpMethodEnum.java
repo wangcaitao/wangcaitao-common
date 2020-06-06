20b29c1f1095a4bcd02c23290925b3bf5a181e9b
wangcaitao <wangcaitao@aliyun.com>
2020-06-06 14:00:02 +0800

    feat(constant): 增加请求方式枚举

---
+++ src/main/java/com/wangcaitao/common/constant/HttpMethodEnum.java
@@ -0,0 +1,54 @@
+package com.wangcaitao.common.constant;
+
+/**
+ * @author wangcaitao
+ */
+public enum HttpMethodEnum {
+
+    /**
+     * OPTIONS
+     */
+    OPTIONS,
+
+    /**
+     * GET
+     */
+    GET,
+
+    /**
+     * HEAD
+     */
+    HEAD,
+
+    /**
+     * POST
+     */
+    POST,
+
+    /**
+     * PUT
+     */
+    PUT,
+
+    /**
+     * DELETE
+     */
+    DELETE,
+
+    /**
+     * PATCH
+     */
+    PATCH,
+
+    /**
+     * CONNECT
+     */
+    CONNECT,
+
+    /**
+     * TRACE
+     */
+    TRACE,
+
+    ;
+}
