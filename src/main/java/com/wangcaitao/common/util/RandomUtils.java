8281a491b7bbbce548f1f96c787f47237f5df1e4
wangcaitao <wangcaitao@aliyun.com>
2020-06-07 09:51:07 +0800

    feat(util): 增加区间随机数, 编码可指定前缀

--- src/main/java/com/wangcaitao/common/util/RandomUtils.java
+++ src/main/java/com/wangcaitao/common/util/RandomUtils.java
@@ -5,6 +5,7 @@ import com.wangcaitao.common.constant.IntegerConstant;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 /**
  * 随机数生成工具类
@@ -12,6 +13,11 @@ import java.time.format.DateTimeFormatter;
  * @author wangcaitao
  */
 public class RandomUtils {
+
+    /**
+     * Random instance
+     */
+    private static final Random RANDOM = new Random();
 
     /**
      * 生成指定位数的随机数
@@ -24,12 +30,44 @@ public class RandomUtils {
     }
 
     /**
+     * 随机指定区间. [MIN, MAX]
+     *
+     * @param min 最小值
+     * @param max 最大值
+     * @return 随机数
+     */
+    public static int generateInterval(int min, int max) {
+        return RANDOM.nextInt(max - min + 1) + min;
+    }
+
+    /**
+     * 随机指定区间. [MIN, MAX)
+     *
+     * @param min 最小值
+     * @param max 最大值
+     * @return 随机数
+     */
+    public static double generateInterval(double min, double max) {
+        return Math.random() * max % (max - min) + min;
+    }
+
+    /**
      * 生成通用编号 (25 位) yyyyMMddHHmmssSSS + 8 位随机数
      *
      * @return 编号
      */
     public static String generateCommonNo() {
         return generateCustomNo(DateTimeFormatterConstant.DATE_TIME_PATTERN_08, IntegerConstant.EIGHT);
+    }
+
+    /**
+     * 生成通用编号 (前缀位数 + 25 位) 前缀 + yyyyMMddHHmmssSSS + 8 位随机数
+     *
+     * @param prefix 前缀
+     * @return 编号
+     */
+    public static String generateCommonNo(String prefix) {
+        return prefix + generateCommonNo();
     }
 
     /**
@@ -41,5 +79,26 @@ public class RandomUtils {
      */
     public static String generateCustomNo(String pattern, Integer digit) {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern)) + generateRandomDigit(digit);
+    }
+
+    /**
+     * 自定义生成编号
+     *
+     * @param prefix  前缀
+     * @param pattern 日期格式
+     * @param digit   随机数字位数
+     * @return 编号
+     */
+    public static String generateCustomNo(String prefix, String pattern, Integer digit) {
+        return prefix + generateCustomNo(pattern, digit);
+    }
+
+    /**
+     * 验证码. 6 位
+     *
+     * @return 验证码
+     */
+    public static String generateCaptcha() {
+        return String.valueOf(generateRandomDigit(IntegerConstant.SIX));
     }
 }
