6bbedc141cc95707bce51aa21f409fffb502eda1
wangcaitao <wangcaitao@aliyun.com>
2020-06-13 13:24:34 +0800

    feat(common): 增加公共正则表达式

---
+++ src/main/java/com/wangcaitao/common/constant/CommonRegexConstant.java
@@ -0,0 +1,14 @@
+package com.wangcaitao.common.constant;
+
+/**
+ * 公共正则表达式
+ *
+ * @author wangcaitao
+ */
+public class CommonRegexConstant {
+
+    /**
+     * 手机号码
+     */
+    public static final String PHONE_NUMBER = "^1[3456789]\\d{9}$";
+}
