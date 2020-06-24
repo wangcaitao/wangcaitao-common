e21f0e05dc06c32f3ef23abe42e9ed8062fcb2f1
wangcaitao <wangcaitao@aliyun.com>
2020-06-24 14:47:13 +0800

    feat(common): 增加公共正则表达式

--- src/main/java/com/wangcaitao/common/constant/CommonRegexConstant.java
+++ src/main/java/com/wangcaitao/common/constant/CommonRegexConstant.java
@@ -11,4 +11,9 @@ public class CommonRegexConstant {
      * 手机号码
      */
     public static final String PHONE_NUMBER = "^1[3456789]\\d{9}$";
+
+    /**
+     * 邮箱
+     */
+    public static final String EMAIL = "^([a-zA-Z0-9\\_\\-\\.]+)\\@([a-zA-Z0-9\\_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
 }
